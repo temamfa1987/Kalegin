683142b6a49a3fc77f0429b7a1b2ebd1f2f1939e
Kalegin <artemkalegin72@gmail.com>
2020-10-02 08:38:18 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kalegin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Kalegin Project
